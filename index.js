73c63cd1075a8ca2e2cf307949db44d446a60db1
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2025-06-24 19:56:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/", (_, res) => res.send("¡Deploy automatizado OK! todo bien"));
+app.get("/", (_, res) => res.send("¡Deploy automatizado OK! todo bien. prueba 3"));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
